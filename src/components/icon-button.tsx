df98b4d3a7760af31d18dce3e2cdba061494c3c6
Joao Piologo <joaotpiologo@hotmail.com>
2025-02-18 22:39:50 -0300

    patch

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,13 +1,11 @@
-import { ReactNode } from "react"
+import { ComponentProps, ReactNode } from "react"
 
-interface IconButtonProps {
-    children: ReactNode //componente filho de tipagem genérica
-}
+interface IconButtonProps extends ComponentProps<'button'>{}
 
 export function IconButton(props: IconButtonProps) {
     return ( 
-        <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-            {props.children}
-        </button>
+        <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+            {...props}
+        />     
     )
 }
