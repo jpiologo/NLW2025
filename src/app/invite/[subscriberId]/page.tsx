a6893ceecf468091fb978fb28a4a6868a9ad68ba
Joao Piologo <joaotpiologo@hotmail.com>
2025-02-22 13:49:05 -0300

    patch finished frontend

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+import logo from '../../../assets/logo.svg'
+import { InviteLinkInput } from './invite-link-input'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
-import { InviteLinkInput } from './invite-link-input'
 
-export default function InvitePage() {
+interface InvitePageProps {
+	params: Promise<{
+		subscriberId: string
+	}>
+}
 
-	const inviteLink = 'http://localhost:3000/'
+export default async function InvitePage(props: InvitePageProps) {
+	const { subscriberId } = await props.params
+
+	const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -35,12 +42,11 @@ export default function InvitePage() {
 
 					<InviteLinkInput inviteLink={inviteLink} />
 
-					<Stats/>
+					<Stats subscriberId={subscriberId} />
 				</div>
 			</div>
 
 			<Ranking />
-			
 		</div>
 	)
 }
